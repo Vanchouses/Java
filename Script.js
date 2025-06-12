47603468230e631e118c88abc440f5ca539d4926
Vanchouses <ivan_shevelev2007@mail.ru>
2025-06-12 18:34:48 +0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -48,3 +48,24 @@ alert(factorial(5));*/
 
 //Задача 3
 
+/*function fib (n) {
+    if (n == 1 || n == 2) {
+        return 1
+    }
+    else {
+        return fib(n-1) + fib(n-2)
+    }
+}
+
+function fib(n) {
+  let a = 1;
+  let b = 1;
+  for (let i = 3; i <= n; i++) {
+    let c = a + b;
+    a = b;
+    b = c;
+  }
+  return b;
+}
+
+alert (fib(77));*/
