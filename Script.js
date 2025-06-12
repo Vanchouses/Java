75d534bac4970b6a08b83d401f6be5f30eec58c2
Vanchouses <ivan_shevelev2007@mail.ru>
2025-06-12 18:17:31 +0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -34,3 +34,17 @@ obj.sayHello();*/
 alert(sumTo(100));*/
 
 //Задача 2
+
+/*function factorial (n) {
+    if (n == 1) {
+        return 1
+    }
+    else {
+        return n * factorial(n-1)
+    }
+}
+
+alert(factorial(5));*/
+
+//Задача 3
+
