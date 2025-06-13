6781712683a8b3776e06bc83d97d2420cb394a11
Vanchouses <ivan_shevelev2007@mail.ru>
2025-06-13 11:07:07 +0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -69,3 +69,39 @@ function fib(n) {
 }
 
 alert (fib(77));*/
+
+//Задача 4
+
+/*let list = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: null
+      }
+    }
+  }
+};
+
+function printList(list) {
+  let tmp = list;
+
+  while (tmp) {
+    alert(tmp.value);
+    tmp = tmp.next;
+  }
+
+}
+
+function printList(list) {
+    alert (list.value);
+
+    if (list.next) {
+        printList(list.next);
+    }
+}
+
+printList(list);*/
