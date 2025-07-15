540a85bbecb2d101a8d11c9fecfe841ca07a48f0
Vanchouses <ivan_shevelev2007@mail.ru>
2025-07-15 11:40:08 +0300

    Добавил...
    Проверка класса: "instanceof"
    Примеси

--- Script.js
+++ Script.js
@@ -723,3 +723,88 @@ let machine = new CoffeeMachine();
 machine.waterAmount = 100;
 alert(machine.#waterAmount);*/
 
+// Проверка класса: "instanceof"
+
+// class Rabbit {}
+// let rabbit = new Rabbit ();
+
+
+// alert (rabbit instanceof Rabbit);
+
+// class Animal {
+//   static [Symbol.hasInstance](obj) {
+//     if (obj.canEat) return true
+//   }
+// }
+
+// let obj = {canEat: true};
+// alert(obj instanceof Animal);
+
+// Примеси
+
+// let sayHiMixin = {
+
+//   sayHi () {
+//     alert(`Привет ${this.name}`);
+//   },
+
+//   sayBye () {
+//     alert(`Пока ${this.name}`);
+//   }
+// };
+
+// class User {
+//   constructor (name){
+//     this.name = name;
+//   }
+// }
+
+// Object.assign(User.prototype, sayHiMixin);
+
+// new User("Петя").sayHi();
+
+
+let eventMixin = {
+
+  on (eventName, handler) {
+    if (!this._eventHandlers) this._eventHandlers = {};
+    if (!this._eventHandlers[eventName]) {
+      this._eventHandlers[eventName] = [];
+    }
+    this._eventHandlers[eventName].push(handler);
+  },
+
+  off (eventName, handler) {
+    let handlers = this._eventHandlers?.[eventName];
+    if (!handlers) return;
+    for (let i = 0; i < handlers.length; i++) {
+      if (handlers[i] === handler) {
+        handlers.splice(i--, 1)
+      }
+    }
+  },
+
+  trigger (eventName, ...args) {
+    if (!this._eventHandlers?.[eventName]) {
+      return;
+    }
+
+    this._eventHandlers[eventName].forEach(handler => handler.apply(this, args));
+  }
+}
+
+class Menu {
+  choose (value) {
+    this.trigger("select", value);
+  }
+}
+
+Object.assign(Menu.prototype, eventMixin);
+
+let menu = new Menu();
+
+menu.on("select", value => alert(`Выбранное значение ${value}`));
+
+menu.choose("JavaScript");
+
+
